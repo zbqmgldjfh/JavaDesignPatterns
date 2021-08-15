f6368bd43195a7c983553ed5c4e726e95c6a8c7c
zbqmgldjfh <zbqmgldjfh@naver.com>
2021-08-15 11:51:52 +0900

    file move

--- src/com/company/Strategy/Main.java
+++ src/com/company/Strategy/Main.java
@@ -1,9 +1,4 @@
-package com.company;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
-import sun.nio.ch.sctp.SctpNet;
-
-import java.io.*;
-import java.util.*;
+package com.company.Strategy;
 
 public class Main {
     public static void main(String[] args) {
