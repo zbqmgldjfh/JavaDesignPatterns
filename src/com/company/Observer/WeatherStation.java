2314ad27b82a9e327f965b9e49d572092860885f
zbqmgldjfh <zbqmgldjfh@naver.com>
2021-08-15 17:27:42 +0900

    내장함수를 사용하지 않음. Sebject에서 push방식을 이용

---
+++ src/com/company/Observer/WeatherStation.java
@@ -0,0 +1,14 @@
+package com.company.Observer;
+
+public class WeatherStation {
+    public static void main(String[] args){
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDIsplay curDisplay = new CurrentConditionsDIsplay(weatherData);
+        HeatIndexDisplay hitDisplay = new HeatIndexDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 30.4f);
+        weatherData.setMeasurements(77, 77, 20.4f);
+    }
+}
--- src/com/company/Strategy/Main.java
+++ src/com/company/Strategy/Main.java
@@ -1,7 +1,7 @@
 package com.company.Strategy;
 
 public class Main {
-    public static void main(String[] args) {
+    private static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
