b1bc77c4338e97caada61f14afbbf2c56af13066
zbqmgldjfh <zbqmgldjfh@naver.com>
2021-08-15 19:30:47 +0900

    내장함수를 사용하지 않음. Sebject에서 push방식을 이용

--- src/com/company/Observer/WeatherStation.java
+++ src/com/company/Observer/WeatherStation.java
@@ -5,7 +5,7 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionsDIsplay curDisplay = new CurrentConditionsDIsplay(weatherData);
-        HeatIndexDisplay hitDisplay = new HeatIndexDisplay(weatherData);
+        //HeatIndexDisplay hitDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 30.4f);
