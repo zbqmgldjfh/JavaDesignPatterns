2a2288df72c5bd023656ba6ea94f4a435b358af5
zbqmgldjfh <zbqmgldjfh@naver.com>
2021-08-29 14:53:58 +0900

    Iterator를 통한 메뉴 추가

--- src/com/company/Iterator/Waitress.java
+++ src/com/company/Iterator/Waitress.java
@@ -5,21 +5,25 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator dinerIterator = dinerMenu.createIterator();
+        Iterator cafeIterator = cafeMenu.createIterator();
 
         System.out.println("메뉴\n----\n아침 메뉴");
         printMenu(pancakeIterator);
         System.out.println("\n점심 메뉴");
         printMenu(dinerIterator);
-
+        System.out.println("\n저녁 메뉴");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator iterator) {
